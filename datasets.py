dbf08f77de447a0f127ab154078142b1d4fb0582
Kitsunetic <kitsune.guru@gmail.com>
2020-04-16 16:41:18 +0900

    Update dataset surface

--- datasets.py
+++ datasets.py
@@ -58,15 +58,28 @@ class LetsGoHikingDataset(torch.utils.data.Dataset):
      13  | DPR 위도
      14  | 강수량 (mm/h, 결측치는 -9999.xxx 형태의 float 값으로 표기) (TARGET)
     """
-    data = custom_transforms(data)
+    #data = custom_transforms(data)
     data = self.tf(data)
     
     temperature = data[:9, ...]
+    """
+    means = [197.3028, 139.9293, 217.1051, 169.6790, 239.5916, 233.3362, 192.1457, 264.3871, 245.8586]
+    stds = [10.2953, 17.4017, 10.1295, 17.8440, 8.0644, 8.8708, 17.2205, 6.1096, 11.2169]
+    for i in range(9):
+      temperature[i, ...] = (temperature[i, ...] - means[i]) / stds[i]
+    """
+    
     surface = data[9, ...]
-    surface = surface.view((1, *surface.shape))
-    surface[surface > 20] = 0
-    surface[surface > 10] -= 10
-    out = torch.cat([temperature, surface], dim=0)
+    surface[surface > 100] = 0.8
+    surface[surface != 0.8] = 1
+    
+    means = [197.3028, 139.9293, 217.1051, 169.6790, 239.5916, 233.3362, 192.1457, 264.3871, 245.8586]
+    for i in range(9):
+      temperature[i, ...] *= surface
+      temperature[i, ...] /= means[i]*0.8
+    
+    #out = torch.cat([temperature, surface], dim=0)
+    out = temperature
     
     if self.is_train:
       target = data[14, ...]
@@ -74,6 +87,8 @@ class LetsGoHikingDataset(torch.utils.data.Dataset):
       
       # Remove NaN
       target[target <= -9000] = 0
+      
+      #target = (target - 0.1457) / 0.6971
       
       return target, out
     else:
